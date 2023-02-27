c8d6ef2df7bd439245e4da1e1c366e638307bb54
Bartlomiej95 <panfilbartlomiej@gmail.com>
2023-02-27 18:10:47 +0100

    Added cookie-parser to project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+  app.use(cookieParser());
+  
+  await app.listen(3001);
 }
 bootstrap();
