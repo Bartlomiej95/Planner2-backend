65ec803826040213ee1e53449bb6e7117481764d
Bartlomiej95 <panfilbartlomiej@gmail.com>
2023-03-01 19:08:21 +0100

    Added getUser function for new http request get('/:id')

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Inject } from '@nestjs/common';
+import { Controller, Get, Inject, Param, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
+import { User } from './entities/user.entity';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -6,4 +8,12 @@ export class UserController {
     constructor(
         @Inject(UserService) private userService: UserService,
       ) {}
+
+      @Get('/:id')
+      @UseGuards(JwtAuthGuard)
+      async getUser(
+        @Param('id') id: string
+      ): Promise<User> {
+        return this.userService.getUser(id)
+      }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common/exceptions';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(){ }
+
+    async getUser(id: string): Promise<User> {
+        if(!id) throw new BadRequestException();
+
+        const user = await User.findOne( { where: { id }});
+        if(!user) throw new NotFoundException();
+
+        return user;
+    }
+}
