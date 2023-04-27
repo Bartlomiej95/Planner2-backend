b6b08b0b2df7d5bd1a2d354589604c03b834795b
Bartlomiej95 <panfilbartlomiej@gmail.com>
2023-04-27 20:55:49 +0200

    Updated activation dto and create-company dto

--- src/auth/dto/activation.dto.ts
+++ src/auth/dto/activation.dto.ts
@@ -2,12 +2,8 @@ import { Position } from "src/types/user.type";
 
 export class ActivationDto {
     urlCode: string;
-    email: string;
     password: string;
     firstName: string;
     lastName: string;
-    ifUserHasCompany: boolean;
-    company: string;
-    nip: number | null;
     position: Position;
 }
--- src/company/dto/create-company.dto.ts
+++ src/company/dto/create-company.dto.ts
@@ -1,4 +1,5 @@
 export class CreateCompanyDto {
     name: string;
     nip: number;
+    urlCode: string;
 }
